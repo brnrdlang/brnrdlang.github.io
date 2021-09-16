5ed996b7f267871dcacfe467015c8533c72330c4
Bernhard Lang <collurio229@gmx.de>
2021-09-16 19:50:41 +0200

    favicon and Gestalt is integrated now

---
+++ resources/js/gestalt.d.ts
@@ -0,0 +1,38 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export class WebGlCanvas {
+  free(): void;
+/**
+* @param {string} canvas_id
+* @returns {WebGlCanvas}
+*/
+  static new(canvas_id: string): WebGlCanvas;
+/**
+* @param {number} time
+*/
+  render(time: number): void;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_webglcanvas_free: (a: number) => void;
+  readonly webglcanvas_new: (a: number, b: number) => number;
+  readonly webglcanvas_render: (a: number, b: number) => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_exn_store: (a: number) => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
